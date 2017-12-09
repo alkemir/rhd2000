f555f7fcede7b6e6e68b476acef885606d7a49e4
Bryan Reynaert <br@akori.cl>
2017-12-09 18:37:42 -0300

    Basic functions working

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from ctypes import cdll
+import ctypes
 
-rhd2klib = cdll.LoadLibrary('./librhd2k.so')
+rhd2klib = ctypes.cdll.LoadLibrary('./librhd2k.so')
+
+rhd2klib._new.restype = ctypes.c_void_p
+rhd2klib.open.argtypes = [ctypes.c_void_p]
+
+class EvalBoard:
+	def __init__(self):
+		self.pointer = rhd2klib._new()
+
+	def open(self):
+		return rhd2klib.open(self.pointer)
+
+
+b = EvalBoard()
+b.open()
--- make.md
+++ make.md
@@ -1,2 +1,3 @@
-g++ -c -fPIC python_wrapper.c -o librhd2k.o
+g++ -c -fPIC python_wrapper.cpp -o librhd2k.o
+g++ -shared -o librhd2k.so librhd2k.o rhd2000evalboard.cpp okFrontPanelDLL.cpp rhd2000datablock.cpp
 g++ -shared -o librhd2k.so librhd2k.o *.cpp
